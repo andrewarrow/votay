5384d1bc8f010804053bd5a7522e08556696d17f
aa <aa@localhost.(none)>
2009-09-15 06:05:04 -0700

    added permalink

--- admin/admin.py
+++ admin/admin.py
@@ -24,6 +24,14 @@ class CreatePostHandler(webapp.RequestHandler):
                  preview='More testing',
                  author_key='',
                  author_name='Andrew Arrow')
+    month = str(bp.created_at.month)
+    if len(month) == 1:
+      month = '0' + month
+    day = str(bp.created_at.day)
+    if len(day) == 1:
+      day = '0' + day
+
+    bp.permalink = str(bp.created_at.year) + '/' + month + '/' + day + '/this-is-a-test-title/'
     bp.put()
     self.redirect('/admin', permanent=False)
     
