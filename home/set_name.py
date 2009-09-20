e5a29d429baab189f75ee4dacce47e03cdcd1cbd
aa <aa@localhost.(none)>
2009-09-20 10:07:11 -0700

    logout

--- home/set_name.py
+++ home/set_name.py
@@ -26,12 +26,23 @@ class SetNameHandler(webapp.RequestHandler):
       nickname.nickname = self.request.get('name')
       nickname.url = self.request.get('url')
     nickname.put()
-    self.redirect(self.request.get('return_url'), permanent=False)
-    
+    if self.request.get('return_url'):
+      self.redirect(self.request.get('return_url'), permanent=False)
+    else:
+      self.redirect('/', permanent=False)      
   def get(self):
     user = users.get_current_user() 
+    query = db.GqlQuery('SELECT * FROM Nickname WHERE user_id = :1', user.user_id())
+    list = query.fetch(1)
+    nickname = None
+    if len(list) == 1:
+      nickname = list[0]
 
-    data = { 'title': 'Set Name', 'return_url': self.request.get('return_url') }
+    data = { 'title': 'Set Name', 
+             'nickname': nickname,
+             'email': user.email(),
+             'logout_url': users.create_logout_url('/'),
+             'return_url': self.request.get('return_url') }
     self.response.out.write(template.render('views/set_name.html', data))
     
 def main():
