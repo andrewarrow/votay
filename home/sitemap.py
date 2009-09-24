cb9bfe634b931400415aa864da7c4a1250e134dc
aa <aa@localhost.(none)>
2009-09-23 17:56:07 -0700

    sitemap

--- home/sitemap.py
+++ home/sitemap.py
@@ -3,17 +3,25 @@ from google.appengine.ext import webapp
 from google.appengine.ext import db
 from google.appengine.api import users
 from google.appengine.ext.webapp import template
-import os
-import sys
+import models
+import util
 
 class MainHandler(webapp.RequestHandler):
   def get(self,path):
-    self.response.headers['Content-Type'] = 'text/plain'
-    self.response.out.write('User-agent: *\n')
-    self.response.out.write('Sitemap: http://www.votay.com/sitemap.xml')
+    util.send404(self)
+    return
+    
+class SiteMapHandler(webapp.RequestHandler):
+  def get(self):
+
+    data = util.getRecentBlogPosts(1)
+    
+    self.response.headers['Content-Type'] = 'text/xml'
+    self.response.out.write(template.render('views/sitemap.xml', data))
     
 def main():
-  application = webapp.WSGIApplication([('/(.*)', MainHandler)
+  application = webapp.WSGIApplication([('/sitemap.xml', SiteMapHandler),
+                                        ('/(.*)', MainHandler)
                                         ],
                                        debug=True)
   wsgiref.handlers.CGIHandler().run(application)
