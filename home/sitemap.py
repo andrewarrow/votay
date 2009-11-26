5f6da6aa6b2698583672b072fd99f58ef2c74f16
aa <aa@localhost.(none)>
2009-11-26 11:23:33 -0800

    sitemaps export

--- home/sitemap.py
+++ home/sitemap.py
@@ -18,9 +18,33 @@ class SiteMapHandler(webapp.RequestHandler):
     
     self.response.headers['Content-Type'] = 'text/xml'
     self.response.out.write(template.render('views/sitemap.xml', data))
+
+class ExportHandler(webapp.RequestHandler):
+  def get(self):
+
+    page = int(self.request.get('p'))
+    query = db.GqlQuery('SELECT * FROM BlogPost ORDER BY created_at desc')
+    posts = query.fetch(1, page-1)
+    
+    if len(posts) == 0:
+      util.send404(self)
+      return    
+    
+    post = posts[0]
+    self.response.headers['Content-Type'] = 'text/plain'    
+    self.response.out.write(post.image + '\n')
+    self.response.out.write(post.title + '\n')
+    self.response.out.write(post.permalink + '\n')
+    self.response.out.write(str(post.created_at)[0:10] + '\n')
+    self.response.out.write(post.author_permalink + '\n')
+    self.response.out.write(post.author_name + '\n')
+    self.response.out.write('<markup>\n')
+    self.response.out.write(post.markup + '\n')  
+    self.response.out.write('</markup>\n')
     
 def main():
   application = webapp.WSGIApplication([('/sitemap.xml', SiteMapHandler),
+                                        ('/export', ExportHandler),
                                         ('/(.*)', MainHandler)
                                         ],
                                        debug=True)
