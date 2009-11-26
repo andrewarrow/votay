4cfabb1e2f2a4c0d5c11e831af24ca2e018dc590
aa <aa@localhost.(none)>
2009-11-26 12:20:25 -0800

    more backup

--- backup.rb
+++ backup.rb
@@ -9,6 +9,10 @@ require 'rubygems'
 require 'net/http'
 require 'uri'
 require 'pp'
+require 'ftools'
+
+File.makedirs('backup_files/posts')
+File.makedirs('backup_files/images')
 
 domain = 'votay.com'  # change to your domain
 
@@ -17,4 +21,20 @@ req = Net::HTTP::Get.new('/export?p=1')
 res = Net::HTTP.start(url.host, url.port) {|http|
   http.request(req)
 }
-puts res.body
+puts res.class == Net::HTTPOK
+
+f = File.open("backup_files/posts/p1.txt", "w")
+f << res.body
+f.close
+
+image = res.body.split("\n").first
+pp image
+
+req = Net::HTTP::Get.new("/blog-image/#{image}")
+res = Net::HTTP.start(url.host, url.port) {|http|
+  http.request(req)
+}
+
+f = File.open("backup_files/images/#{image}", "w")
+f << res.body
+f.close
